70197730cf7e756fdd5b34e1e2c63b0575b60273
joserafaelSH <56702181+joserafaelSH@users.noreply.github.com>
2022-08-27 17:37:06 -0300

    Time: 47 ms (94.25%), Space: 14 MB (82.74%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.py
+++ 162-find-peak-element/162-find-peak-element.py
@@ -7,23 +7,24 @@ class Solution:
         s0 = 0
         s = len(nums)-1
         
-        while s0 <= s:
+        while s0 < s:
             mid = (s+s0)//2
             
-            if mid == len(nums)-1:
-                if nums[mid] > nums[mid-1] :
-                    return mid
+#             if mid == len(nums)-1:
+#                 if nums[mid] > nums[mid-1] :
+#                     return mid
             
-            if mid == 0:
-                if nums[mid] > nums[mid+1] :
-                    return mid
+#             if mid == 0:
+#                 if nums[mid] > nums[mid+1] :
+#                     return mid
             
-            if nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
-                return mid
+#             if nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
+#                 return mid
 
-            if nums[mid] < nums[mid-1]:
-                s = mid-1
-            else:
+            if nums[mid] < nums[mid+1]:
+               
                 s0 = mid+1 
+            else:
+                 s = mid
 
-        return len(nums)-1
+        return s0
