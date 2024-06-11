f9171b0160516aa74d3ee37a75f06b4a4aaf6b52
joserafaelSH <56702181+joserafaelSH@users.noreply.github.com>
2024-06-11 13:22:34 -0300

    Time: 39 ms (68.91%), Space: 16.6 MB (93.48%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.py
+++ 1217-relative-sort-array/1217-relative-sort-array.py
@@ -1,18 +1,20 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         
-      
-        aux = []
+        memo = [0 for i in range(1001)]
         response = []
-
         for i in arr1:
-            if i not in arr2:
-                aux.append(i)
-
+            memo[i]+=1
+        
         for i in arr2:
-            for k in arr1:
-                if k == i:
-                    response.append(k)
+            response += [i for k in range(memo[i])]
+            memo[i] = 0
+
+        aux = []
+        for i in range(len(memo)):
+            if memo[i] != 0:
+                aux+=[i for k in range(memo[i])]
+
 
         aux.sort()
 
